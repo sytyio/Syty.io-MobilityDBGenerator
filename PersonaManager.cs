32caba300862f61f30652969fc5443ca51b6eb4e
Sebastien Collette <sco@synapsis-group.com>
2022-03-03 10:27:13 +0000

    Completed version.

--- PersonaManager.cs
+++ PersonaManager.cs
@@ -3,7 +3,7 @@ namespace Generator
     class PersonaManager
     {
         private Random rand = new Random();
-        const int batchSize = 100;
+        const int batchSize = 1000;
         public PersonaManager()
         {
 
@@ -19,11 +19,6 @@ namespace Generator
                 }
                 for (int i = 0; i < count; i++)
                 {
-                    if (i % batchSize == 0)
-                    {
-                        Console.WriteLine(".");
-                        sql.WriteLine("INSERT INTO persona (home_sector, work_sector, home_location, work_location, gender, age) VALUES ");
-                    }
                     var persona = matrixManager.GetRandomPersona();
                     persona.HomeLocation = geographyManager.GetLocation(persona.HomeSector);
                     persona.WorkLocation = geographyManager.GetLocation(persona.WorkSector);
@@ -37,6 +32,11 @@ namespace Generator
                     }
                     else
                     {
+                        if (i % batchSize == 0)
+                        {
+                            Console.WriteLine(".");
+                            sql.WriteLine("INSERT INTO persona (home_sector, work_sector, home_location, work_location, gender, age) VALUES ");
+                        }
                         sql.Write("('" + persona.HomeSector + "', '" + persona.WorkSector + "', '" + persona.HomeLocation.ToText() + "', '" + persona.WorkLocation.ToText() + "', '" + (rand.Next(2) > 0 ? "Male" : "Female") + "', " + ((int)(rand.NextDouble()*rand.NextDouble()*80+18)) + ")");
                         if (i + 1 == count || (i + 1) % batchSize == 0)
                         {
